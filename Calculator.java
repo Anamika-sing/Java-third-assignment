fe003d660f6383eee2ffa5984cdb2c772d2b36d4
Anamika Singh <sianamika123@gmail.com>
2025-04-05 11:37:41 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+
+        int a = 10;
+        int b = 5;
+
+        System.out.println("Addition: " + calc.add(a, b));
+        System.out.println("Subtraction: " + calc.subtract(a, b));
+        System.out.println("Multiplication: " + calc.multiply(a, b));
+
+        try {
+            System.out.println("Division: " + calc.divide(a, b));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
